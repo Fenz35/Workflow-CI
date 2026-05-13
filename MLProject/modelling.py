476cae3fbc86538466631f6c3d66fe16ca7469aa
Fenz35 <fendy.strife22@gmail.com>
2026-05-14 06:30:14 +0700

    Edit script agar CI jalan

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -27,33 +27,33 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 input_example = X_train.iloc[:5]
 
-with mlflow.start_run(run_name="GBR_Default_Run"):
-    mlflow.autolog(log_models=False)
-    model = GradientBoostingRegressor(n_estimators=100, random_state=42)
+mlflow.autolog(log_models=False)
 
-    model.fit(X_train, y_train)
-       
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-    )
+model = GradientBoostingRegressor(n_estimators=100, random_state=42)
+
+model.fit(X_train, y_train)
     
-    
-    # kalkulasi metrik pada data testing
-    predictions = model.predict(X_test)
-    
-    r2 = model.score(X_test, y_test)
-    mae = mean_absolute_error(y_test, predictions)
-    mse = mean_squared_error(y_test, predictions)
-    rmse = np.sqrt(mse)
-    
-    # Explicitly log additional metrics to tracking UI
-    mlflow.log_metric("testing_r2_score", r2)
-    mlflow.log_metric("testing_mae", mae)
-    mlflow.log_metric("testing_rmse", rmse)
-    
-    print("--- Training Metrics ---")
-    print(f"R2 Score : {r2:.4f}")
-    print(f"MAE      : {mae:.4f} (Average off by {mae:.2f} grade points)")
-    print(f"RMSE     : {rmse:.4f}")
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model",
+    input_example=input_example
+)
+
+
+# kalkulasi metrik pada data testing
+predictions = model.predict(X_test)
+
+r2 = model.score(X_test, y_test)
+mae = mean_absolute_error(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+
+# Explicitly log additional metrics to tracking UI
+mlflow.log_metric("testing_r2_score", r2)
+mlflow.log_metric("testing_mae", mae)
+mlflow.log_metric("testing_rmse", rmse)
+
+print("--- Training Metrics ---")
+print(f"R2 Score : {r2:.4f}")
+print(f"MAE      : {mae:.4f} (Average off by {mae:.2f} grade points)")
+print(f"RMSE     : {rmse:.4f}")
